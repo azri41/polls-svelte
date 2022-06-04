e30f4518c93a55605bfeca4d79450f59dd0f5789
azri41 <azri041@gmail.com>
2022-06-04 08:22:54 +0800

    second commit

--- src/App.svelte
+++ src/App.svelte
@@ -14,14 +14,14 @@
 
 	  // polls
 	let polls = [
-    {
-      id: 1,
-      question: 'Python or JavaScript?',
-      answerA: 'Python',
-      answerB: 'JavaScript',
-      votesA: 9,
-      votesB: 15,
-    },
+		{
+		id: 1,
+		question: 'Python or JavaScript?',
+		answerA: 'Python',
+		answerB: 'JavaScript',
+		votesA: 9,
+		votesB: 15,
+		},
   	];
 
 	const handleAdd = (e) => {
@@ -30,13 +30,29 @@
 		activeItem = 'Current Polls';
 	}
 
+	const handleVote = (e) => {
+		const { id, option } = e.detail;
+
+		let copiedPolls = [...polls];
+		let upvotedPoll = copiedPolls.find((poll) => poll.id == id);
+
+		if (option === 'a'){
+			upvotedPoll.votesA++;
+		}
+		if (option === 'b'){
+			upvotedPoll.votesB++;
+		}
+
+		polls = copiedPolls;
+	}
+
 </script>
 
 <Header />
 <main>
 	<Tabs {activeItem} {items} on:tabChange={tabChange} />
 	{#if activeItem === 'Current Polls'}
-		<PollList polls={polls}/>
+		<PollList polls={polls} on:vote={handleVote}/>
 	{:else if activeItem === 'Add New Poll'}
 		<CreatePollForm on:add={handleAdd}/>
 	{/if}
